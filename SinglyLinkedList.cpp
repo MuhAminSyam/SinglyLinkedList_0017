24aea8148a878454412809252850483110f21d8d
MuhAminSyam <aminsyammuh@gmail.com>
2025-05-08 19:43:23 +0700

    pembuatan logika untuk mengelola data mahasiswa

--- SinglyLinkedList.cpp
+++ SinglyLinkedList.cpp
@@ -125,3 +125,53 @@ int main()
         cout << "5. Keluar\n";
         cout << "\nMasukkan pilihan (1-5): ";
         cin >> ch;
+
+        switch (ch)
+        {
+        case '1':
+            mhs.addNode();
+            break;
+        case '2':
+            if (mhs.listEmpty())
+            {
+                cout << "\nList Kosong" << endl;
+                break;
+            }
+
+            cout << "\nMasukkan nomor mahasiswa yang akan dihapus : ";
+            cin >> nim;
+            if (mhs.delNode(nim) == false)
+                cout << "\nData tidak ditemukan" << endl;
+            else
+                cout << "\nData dengan nomor mahasiswa " << nim << " berhasil dihapus " << endl;
+            break;
+        case '3':
+            mhs.traverse();
+            break;
+        case '4':
+        {
+            if (mhs.listEmpty() == true)
+            {
+                cout << "\nList Kosong\n";
+                break;
+            }
+            Node *previous, *current;
+            cout << "\nMasukkan nomor mahasiswa yang dicari : ";
+            cin >> nim;
+            if (mhs.Search(nim, previous, current) == false)
+                cout << "\nData tidak ditemukan" << endl;
+            else
+            {
+                cout << "\nData ditemukan" << endl;
+                cout << "\nNIM Mahasiswa : " << current->noMhs << endl;
+            }
+            break;
+        }
+        case '5':
+            break;
+        default:
+            cout << "\nPilihan salah!" << endl;
+            break;
+        }
+    } while (ch != '5');
+}
