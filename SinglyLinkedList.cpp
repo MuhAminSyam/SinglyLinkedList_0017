6f7e46203f21548d41dbaabda4cc72fd0e0c59a4
MuhAminSyam <aminsyammuh@gmail.com>
2025-05-08 19:40:58 +0700

    membuat int main

--- SinglyLinkedList.cpp
+++ SinglyLinkedList.cpp
@@ -108,3 +108,20 @@ public:
         }
     }
 };
+
+int main()
+{
+    LinkedList mhs;
+    int nim;
+    char ch;
+
+    do
+    {
+        cout << "\nMenu" << endl;
+        cout << "1. Menambahkan data ke dalam list" << endl;
+        cout << "2. Menghapus data dari dalam list" << endl;
+        cout << "3. Menampilkan semua data dalam list" << endl;
+        cout << "4. Mencari data dalam list" << endl;
+        cout << "5. Keluar\n";
+        cout << "\nMasukkan pilihan (1-5): ";
+        cin >> ch;
